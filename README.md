e5df9eafdc111156b86f1532e3034933c03bc346
amc01 <amc001@ucsd.edu>
2021-11-13 19:35:54 -0800

    unit tests part 2

--- README.md
+++ README.md
@@ -5,4 +5,12 @@
     your code is not the best. You also would not want to test your code after all development has been completed since you won't
     be able to catch the bugs during development. 
 
-2. Yes
+2. No
+
+3. You would not use a unit test for this. You want want to do end to end testing to ensure that a message sent on one end of received
+   on the other end. You might be able to write unit tests to check individual component of the message feature, but the message feature
+   as a whole should not be checked with a unit test.
+
+4. You would use a unit test for max message length. You can check to see if an error is thrown when the message length is longer than
+   80 characters. Since max message length is a component of the message feature, you should use a unit test to see if this part
+   of the message feature works as expected.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,57 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+test('isPhoneNumber(phoneNumber)', () => {
+    let number1 = "11234567899";
+    expect(functions.isPhoneNumber(number1)).toBe(true);
+    let number2 = "15242357849";
+    expect(functions.isPhoneNumber(number2)).toBe(true);
+    let number3 = "12345678991234";
+    expect(functions.isPhoneNumber(number3)).toBe(false);
+    let number4 = "123499";
+    expect(functions.isPhoneNumber(number4)).toBe(false);
+});
+
+test('isEmail(email)', () => {
+    let email1 = "and@gmail.com";
+    expect(functions.isEmail(email1)).toBe(true);
+    let email2 = "asfgjg@yahoo.com";
+    expect(functions.isEmail(email2)).toBe(true);
+    let email3 = "andgmail.com";
+    expect(functions.isEmail(email3)).toBe(false);
+    let email4 = "and@aol";
+    expect(functions.isEmail(email4)).toBe(false);
+});
+
+test('isStrongPassword(password)', () => {
+    let pass1 = "Avjdss3"
+    expect(functions.isStrongPassword(pass1)).toBe(true);
+    let pass2 = "Enfnsd_32"
+    expect(functions.isStrongPassword(pass2)).toBe(true);
+    let pass3 = "Avj#$s3"
+    expect(functions.isStrongPassword(pass3)).toBe(false);
+    let pass4 = "vjjaiugbwiuenfiuasfdss3"
+    expect(functions.isStrongPassword(pass4)).toBe(false);
+});
+
+test('isDate(date)', () => {
+    let date1 = "01 / 11 / 2001";
+    expect(functions.isDate(date1)).toBe(true);
+    let date2 = "12 / 21 / 1934";
+    expect(functions.isDate(date2)).toBe(true);
+    let date3 = "012 / 11 / 201";
+    expect(functions.isDate(date3)).toBe(false);
+    let date4 = "01 /  / 20031";
+    expect(functions.isDate(date4)).toBe(false);
+});
+
+test('isHexColor(color)', () => {
+    let hex1 = "#C4462B";
+    expect(functions.isHexColor(hex1)).toBe(true);
+    let hex2 = "#fc9";
+    expect(functions.isHexColor(hex2)).toBe(true);
+    let hex3 = "#C44f462B";
+    expect(functions.isHexColor(hex3)).toBe(false);
+    let hex4 = "#f9";
+    expect(functions.isHexColor(hex4)).toBe(false);
+});
